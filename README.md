3081922e578b6627bb13a16397f83da9cb264858
Aureylio <aureyliorangga2@gmail.com>
2025-10-22 13:48:33 +0700

    Modifikasi Percobaan 1

---
+++ README.md
@@ -0,0 +1,24 @@
+PERCOBAAN 1
+1. komponen perulangan FOR pada percobaan 1
+jawab : 
+> inisialisasi : int i = 1 -men-set variabel penghitung sebelum loop mulai
+> kondisi      : i <= 1    -selama kondisi true, loop berjalan
+> update       : i++       -dieksekusi setiap akhir literasi untuk memperbarui penghitung
+
+2. kenapa tertinggi 0 dan terendah 100
+jawab :
+alasannya : Nilai siswa yang di asumsikan di rentang 0-100.
+> tertinggi : diinisialisasi 0 supaya setiap nilai valid (>0) dapat menggantikan nilai tertinggi awal jika lebih besar.
+> terendah  : diinisialisasi 100 supaya setiap nilai valid (<100>) dapat menggantikan terendah awal jika lebih kecil.
+
+3. fungsi alur kerja dari potongan berikut
+jawab :
+> Fungsinya untuk membandingkan nilai yang baru di input dengan nilai maksimum/minimum saat ini.
+> Alur : 
+  1. input nilai siswa (setiap literasi for)
+  2. cek apakah nilai lebih besar dari tertinggi. jika ya, update tertinggi.
+  3. cek apakah nilai lebih kecil dari terendah. jika ya, update terendah.
+  4. setelah loop selesai, variabel tertinggi dan terendah memulai nilai maksimum/minimum dari seluruh input. 
+
+4. Modifikasi hitung jumlah mahasiswa
+jawab : commit
--- SiakadFor07.java
+++ SiakadFor07.java
@@ -3,21 +3,34 @@ public class SiakadFor07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
      
-        double niai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-            niai = sc.nextDouble();
-            if (niai > tertinggi) {
-                tertinggi = niai;
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
             }
-            if (niai < terendah) {
-                terendah = niai;
-            } 
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+
+            //hitung lulus atau tidak lulus (modifikasi no 4 batasan kelulusan 60)
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
+    
+
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jumlahTidakLulus);
+    
         sc.close();
     }
 }
